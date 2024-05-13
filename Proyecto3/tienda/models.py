d60ad4ff77462f688db7ad7d826d4419dc131b97
Constanza Perez <krcelty@gmail.com>
2024-05-12 21:32:52 -0400

    tienda: crear modelos

--- Proyecto3/tienda/models.py
+++ Proyecto3/tienda/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Mascota(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+    
+class Dueño(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Tipo(models.Model):
+    nombre = models.CharField(max_length=255)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+class Edad(models.Model):
+    edad = models.IntegerField()
+
+    def __int__(self) -> int:
+        return self.edad
+
+class Peso(models.Model):
+    peso = models.DecimalField(max_digits=2)
+
+class ficha(models.Model):
+    nombre = models.PositiveIntegerField(unique=True)
+    mascota = models.ForeignKey(Mascota, on_delete=models.SET_NULL, null=True, blank=True )
+    dueño = models.ForeignKey(Dueño, on_delete=models.SET_NULL, null=True, blank=True )
+    tipo = models.ManyToManyField(Tipo)
+
+
+
+
+   
+
+
